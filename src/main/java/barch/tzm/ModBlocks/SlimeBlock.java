d72757f63c427dafdc6325cead56dd5bd1551cb1
BarchamMal <115943779+BarchamMal@users.noreply.github.com>
2025-05-07 12:43:08 -0500

    Delete SlimeBlock.java
    oops. that was test code... i think

--- src/main/java/barch/tzm/ModBlocks/SlimeBlock.java
+++
@@ -1,9 +0,0 @@
-package barch.tzm.ModBlocks;
-
-import net.minecraft.block.SnowBlock;
-
-public class SlimeBlock extends SnowBlock {
-    public SlimeBlock(Settings settings) {
-        super(settings);
-    }
-}
